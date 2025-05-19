398d0a3fe5cfb6a5081aa33662a7ddd11441cfdb
GEO <seojw115@postech.ac.kr>
2025-05-19 19:53:17 +0900

    Update electric_capacity.py

--- electric_capacity.py
+++ electric_capacity.py
@@ -4,7 +4,7 @@ elec = []
 '''for i in range(23, 32):
     date = '2024-10-{}'.format(i)
     smp_rt_rc = requests.get(f'https://research-api.solarkim.com/data/cmpt-2024/elec-supply/{date}', headers={
-                                'Authorization': f'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJobk1UVmlkY3hjRVJZM3FWdnpkeEQyIiwiaWF0IjoxNzMwMTU2NzU3LCJleHAiOjE3MzE1OTY0MDAsInR5cGUiOiJhcGlfa2V5In0.2hcgpvv-8Q9n8RHnpgGxTaO6jeAFn8n00ARP9dLWMyY'
+                                'Authorization': 'API-KEY'
                             }).json()
     for j in smp_rt_rc:
         price_list.append(j)'''
@@ -12,7 +12,7 @@ for i in range(13,14):
 
     date = '2024-11-{}'.format(str(i))
     elec_supply = requests.get(f'https://research-api.solarkim.com/data/cmpt-2024/elec-supply/{date}', headers={
-                                'Authorization': f'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJobk1UVmlkY3hjRVJZM3FWdnpkeEQyIiwiaWF0IjoxNzMwMTU2NzU3LCJleHAiOjE3MzE1OTY0MDAsInR5cGUiOiJhcGlfa2V5In0.2hcgpvv-8Q9n8RHnpgGxTaO6jeAFn8n00ARP9dLWMyY'
+                                'Authorization': 'API-KEY'
                             }).json()
     '''for j in smp_rt_rc:
         price_list.append(j)'''
