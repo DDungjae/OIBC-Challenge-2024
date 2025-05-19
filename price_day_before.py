79a5768335c9058ba7ad7a34ae526d9337e04a11
GEO <seojw115@postech.ac.kr>
2025-05-19 19:58:10 +0900

    Update price_day_before.py

--- price_day_before.py
+++ price_day_before.py
@@ -2,6 +2,6 @@ import requests
 
 date = '2024-11-01'
 smp_da = requests.get(f'https://research-api.solarkim.com/data/cmpt-2024/smp-da/{date}', headers={
-                            'Authorization': 'API-TOKEN'
+                            'Authorization': 'API-KEY'
                         }).json()
 print(smp_da) 
